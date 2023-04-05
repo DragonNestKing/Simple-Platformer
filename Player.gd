3326c370cb4c67c165a6a4256e2f133e5add6069
DragonNestKing <116030215+DragonNestKing@users.noreply.github.com>
2023-04-05 14:35:16 -0500

    fixed jump, wall slide and jump, map update
    We fixed the jump glitch. I added a wall slide and wall jump functionality. I also added to the map.

--- Player.gd
+++ Player.gd
@@ -11,21 +11,39 @@ export(int) var ADDITIONAL_FALL_GRAVITY = 4
 var velocity = Vector2.ZERO
 	
 func _physics_process(delta: float) -> void:
-	
-	apply_gravity()
+	if not is_on_wall():
+		apply_gravity(200)
+	elif is_on_wall():
+		apply_gravity(25)
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
+	
 	if input.x ==0:
 		apply_friction()
+		$AnimatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
+		$AnimatedSprite.animation = "Run"
+		if input.x < 0:
+			$AnimatedSprite.flip_h = false
+		elif input.x > 0:
+			$AnimatedSprite.flip_h = true
 		
 	# player jump
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
+	elif is_on_wall() and not is_on_floor():
+		$AnimatedSprite.animation = "On Wall"
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_FORCE
+			if velocity.x > 0:
+				velocity.x = -150
+			elif velocity.x < 0:
+				velocity.x = 150
 	else:
+		$AnimatedSprite.animation = "Jump"
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
 			velocity.y = JUMP_RELEASE_FORCE
 		
@@ -34,12 +52,17 @@ func _physics_process(delta: float) -> void:
 			
 	
 	# apply movement to the player
+	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		$AnimatedSprite.animation = "Run"
+		$AnimatedSprite.frame = 1
 
-func apply_gravity():
+func apply_gravity(power):
 	# gravity
 	velocity.y += GRAVITY
-	velocity.y = min(velocity.y, 200)
+	velocity.y = min(velocity.y, power)
 	
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
